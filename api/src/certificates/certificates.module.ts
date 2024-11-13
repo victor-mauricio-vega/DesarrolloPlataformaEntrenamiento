d7c2e00503f97610c8e65430d7c97ded7f43d58a
Andres Loto <riderandresj@hotmail.com>
2024-11-12 22:15:20 -0500

    creación de controladores

--- api/src/certificates/certificates.module.ts
+++ api/src/certificates/certificates.module.ts
@@ -2,6 +2,6 @@ import { Module } from '@nestjs/common';
 import { CertificateController } from './controllers/certificate/certificate.controller';
 
 @Module({
-  controllers: [CertificateController]
+  controllers: [CertificateController],
 })
 export class CertificatesModule {}
