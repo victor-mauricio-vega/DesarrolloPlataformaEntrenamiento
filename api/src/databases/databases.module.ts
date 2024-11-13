738117efff885e6c899b602736deaa3491387267
Andres Loto <riderandresj@hotmail.com>
2024-11-12 23:08:44 -0500

    Configurando la conexion a la BD

--- api/src/databases/databases.module.ts
+++ api/src/databases/databases.module.ts
@@ -1,4 +1,35 @@
 import { Module } from '@nestjs/common';
+import { ConfigType } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import config from 'src/config';
+import { View_Clases_Estudiantes } from 'src/users/views/clases_estudiantes.view';
 
-@Module({})
+@Module({
+    imports: [
+        TypeOrmModule.forRootAsync({
+          inject: [config.KEY],
+          useFactory: (configService: ConfigType<typeof config>) => {
+            const { server, database, user, password, encrypt, port } =
+              configService.sqlserver;
+    
+            return {
+              type: 'mssql',
+              host: server,
+              database,
+              username: user,
+              password,
+              port,
+              synchronize: false,
+              autoLoadEntities: true,
+              entities: [View_Clases_Estudiantes],
+              extra: {
+                trustServerCertificate: true,
+              },
+            };
+          },
+        }),
+      ],
+      providers: [],
+      exports: [TypeOrmModule],
+})
 export class DatabasesModule {}
