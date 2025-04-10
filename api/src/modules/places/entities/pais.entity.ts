c7c9fdad1d67a27f5656f81fb4f214350c564f7f
jmgalindor9802 <jmgalindor9802@gmail.com>
2025-04-10 10:12:29 -0500

    refactor(banckend): creacion de entity pais

---
+++ api/src/modules/places/entities/pais.entity.ts
@@ -0,0 +1,27 @@
+import { Clase } from './../../classes/entities/clase.entity';
+import { Empresa } from './../../companies/entities/empresa.entity';
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { Ubicacion } from './ubicacion.entity';
+import { Usuario } from '../../users/entities/usuario.entity';
+
+@Entity('PAIS')
+export class Pais {
+  @PrimaryGeneratedColumn({ name: 'PK_PAIS', primaryKeyConstraintName: 'PK_PAIS', comment: 'Clave primaria de la tabla País' })
+  pk_pais: number;
+
+  @Column({ type: 'nvarchar', length: 50, nullable: false, comment: 'Nombre del país', name:'NOMBRE' })
+  nombre: string;
+
+  @OneToMany(() => Usuario, usuario => usuario.pais)
+  usuarios: Usuario[];
+  /*
+  @OneToMany(() => Clase, (clase) => clase.pais_orden_venta)
+  clases: Clase[];
+
+  @OneToMany(() => Empresa, (empresa) => empresa.pais)
+  empresas: Empresa[];
+
+  @OneToMany(() => Ubicacion, (ubicacion) => ubicacion.pais)
+  ubicaciones: Ubicacion[];
+  */
+}
